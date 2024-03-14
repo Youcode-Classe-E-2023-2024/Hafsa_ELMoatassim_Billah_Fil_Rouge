dc39e88cbf4e66db9560daff86c09590af58098e
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-03-14 14:19:06 +0000

    login et signup front

---
+++ FilRouge-app/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('Auth.login');
+});
+
+Route::get('/register', function () {
+    return view('Auth.register');
+});
