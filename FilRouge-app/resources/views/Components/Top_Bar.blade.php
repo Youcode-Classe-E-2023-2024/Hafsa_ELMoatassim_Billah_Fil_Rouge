bd057740b88e6f697ab367d7460fac0db65fcc0d
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-03-18 12:27:27 +0000

    change the color of the navbar

--- FilRouge-app/resources/views/Components/Top_Bar.blade.php
+++ FilRouge-app/resources/views/Components/Top_Bar.blade.php
@@ -1,3 +1,3 @@
-<div class="w-full h-10 bg-yellow-700 bg-opacity-50 text-center pt-3 text-xs">
+<div class="w-full h-10 bg-pink-800 font-sans bg-opacity-50 text-center pt-3 text-xs">
     <p>Free Shipping On Orders Over 60$</p>
 </div>
