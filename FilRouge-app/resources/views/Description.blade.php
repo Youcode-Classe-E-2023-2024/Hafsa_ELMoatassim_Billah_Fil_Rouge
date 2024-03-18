bd84e41ff764fd605fa56a393a5d83f10d3e95a8
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-03-18 10:44:13 +0000

    adding the description

--- FilRouge-app/resources/views/Description.blade.php
+++ FilRouge-app/resources/views/Description.blade.php
@@ -49,7 +49,7 @@
                 </div>
                 <div>
                     <button type="button"
-                            class="h-10 mt-1 ml-5 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2  dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                            class="pr-10 pl-10 h-10 mt-1 ml-5 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2  dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                         Add to Bag
                     </button>
                 </div>
@@ -86,10 +86,28 @@
                 </svg>
 
             </div>
-
         </div>
     </div>
 </div>
+
+<div class="flex items-center">
+    <div class="border-t border-gray-400 my-20 flex-grow"></div>
+    <div class="mx-4 text-green-900 font-regular leading-none"><u>Product Description</u></div>
+    <div class="border-t border-gray-400 flex-grow"></div>
+</div>
+
+<div class="mb-15 w-full md:w-3/5 mx-auto">
+    <p class="text-gray-600">Discover our Green Tea Cleansing Gel, a deep cleansing solution that preserves your skin's natural hydration. Designed to provide a gentle and effective experience, this gel contains carefully selected natural ingredients, such as Camelia Sinensis extract with hydrating, anti-inflammatory and anti-aging properties, as well as Chamomilla Recutita extract, soothing and hydrating .
+        <br><br>
+        Our unique formula incorporates Laurdimonium hydroxypropyl hydrolyzed wheat protein, strengthening the skin barrier and minimizing fine lines. With the gentle surfactant Cocamidopropyl betaine, this gel provides a gentle cleansing experience without the drying effects. Additionally, our product is free of sulfates, parabens, silicones and other harmful chemicals, making it ideal for sensitive skin.
+        <br><br>
+        The generous capacity of 120 ml guarantees long-lasting use. The full INCI list attests to our commitment to quality ingredients, with a concentration of 97% of ingredients of natural origin. We've removed silicones, parabens and sulfates from our formula, making Hazel Gel Cleanser the ideal choice for your skincare routine.
+        <br><br>
+        For all skin types, even the most sensitive, our Cleansing Gel offers a feeling of softness and freshness. Before first use, perform a patch test on a small area, especially if you have atopic skin or dermatological problems. For radiant and healthy skin, adopt Hazel Green Tea Cleansing Gel</p>
+</div>
+
+
+@include('Components.footer')
 </body>
 
 <script>
