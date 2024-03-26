590218cc5d7aaefcdea31586428c4f15d92f7ae3
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-03-26 13:36:32 +0000

    user

--- FilRouge-app/resources/views/Admin/Dashboard.blade.php
+++ FilRouge-app/resources/views/Admin/Dashboard.blade.php
@@ -295,11 +295,7 @@
                                         </th>
                                         <th
                                             class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                                            Title
-                                        </th>
-                                        <th
-                                            class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                                            Status
+                                            Email
                                         </th>
                                         <th
                                             class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
@@ -320,22 +316,15 @@
                                                 </div>
 
                                                 <div class="ml-4">
-                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe
-                                                    </div>
-                                                    <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe</div>
                                                 </div>
                                             </div>
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                             <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                            <div class="text-sm leading-5 text-gray-500">Web dev</div>
                                         </td>
 
-                                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                                <span
-                                                    class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
-                                        </td>
 
                                         <td
                                             class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
@@ -357,22 +346,15 @@
                                                 </div>
 
                                                 <div class="ml-4">
-                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe
-                                                    </div>
-                                                    <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe</div>
                                                 </div>
                                             </div>
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                             <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                            <div class="text-sm leading-5 text-gray-500">Web dev</div>
                                         </td>
 
-                                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                                <span
-                                                    class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
-                                        </td>
 
                                         <td
                                             class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
@@ -394,22 +376,15 @@
                                                 </div>
 
                                                 <div class="ml-4">
-                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe
-                                                    </div>
-                                                    <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe</div>
                                                 </div>
                                             </div>
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                             <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                            <div class="text-sm leading-5 text-gray-500">Web dev</div>
                                         </td>
 
-                                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                                <span
-                                                    class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
-                                        </td>
 
                                         <td
                                             class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
@@ -431,22 +406,15 @@
                                                 </div>
 
                                                 <div class="ml-4">
-                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe
-                                                    </div>
-                                                    <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe</div>
                                                 </div>
                                             </div>
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                             <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                            <div class="text-sm leading-5 text-gray-500">Web dev</div>
                                         </td>
 
-                                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                                <span
-                                                    class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
-                                        </td>
 
                                         <td
                                             class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
@@ -468,22 +436,15 @@
                                                 </div>
 
                                                 <div class="ml-4">
-                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe
-                                                    </div>
-                                                    <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe</div>
                                                 </div>
                                             </div>
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                             <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                            <div class="text-sm leading-5 text-gray-500">Web dev</div>
                                         </td>
 
-                                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                                <span
-                                                    class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
-                                        </td>
 
                                         <td
                                             class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
@@ -505,22 +466,15 @@
                                                 </div>
 
                                                 <div class="ml-4">
-                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe
-                                                    </div>
-                                                    <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe</div>
                                                 </div>
                                             </div>
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                             <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                            <div class="text-sm leading-5 text-gray-500">Web dev</div>
                                         </td>
 
-                                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                                <span
-                                                    class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
-                                        </td>
 
                                         <td
                                             class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
@@ -542,22 +496,15 @@
                                                 </div>
 
                                                 <div class="ml-4">
-                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe
-                                                    </div>
-                                                    <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe</div>
                                                 </div>
                                             </div>
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                             <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                            <div class="text-sm leading-5 text-gray-500">Web dev</div>
                                         </td>
 
-                                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                                <span
-                                                    class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
-                                        </td>
 
                                         <td
                                             class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
@@ -579,22 +526,45 @@
                                                 </div>
 
                                                 <div class="ml-4">
-                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe
-                                                    </div>
-                                                    <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe</div>
                                                 </div>
                                             </div>
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                             <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                            <div class="text-sm leading-5 text-gray-500">Web dev</div>
+                                        </td>
+
+
+                                        <td
+                                            class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
+                                            Owner
+                                        </td>
+
+                                        <td
+                                            class="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-no-wrap border-b border-gray-200">
+                                            <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                            <div class="flex items-center">
+                                                <div class="flex-shrink-0 w-10 h-10">
+                                                    <img class="w-10 h-10 rounded-full"
+                                                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
+                                                         alt="">
+                                                </div>
+
+                                                <div class="ml-4">
+                                                    <div class="text-sm font-medium leading-5 text-gray-900">John Doe</div>
+                                                </div>
+                                            </div>
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                                <span
-                                                    class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
+                                            <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
                                         </td>
+
 
                                         <td
                                             class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
