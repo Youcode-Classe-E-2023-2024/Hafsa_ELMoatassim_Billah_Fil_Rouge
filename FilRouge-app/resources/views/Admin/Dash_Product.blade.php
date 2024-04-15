dbb28e44753b062efe1131a48e48c504865dc048
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-04-15 15:11:18 +0100

    dashboard product

---
+++ FilRouge-app/resources/views/Admin/Dash_Product.blade.php
@@ -0,0 +1,9 @@
+@include('Partials.layout')
+<div>
+    @include('Partials.dashnav')
+
+    <main class="flex-1 overflow-x-hidden overflow-y-auto" style="background-color: #d4e6c8;">
+        <div class="container px-6 py-8 mx-auto">
+
+
+        </div>
