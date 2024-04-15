0440987bf5dd65ad10080177ad55cee5dbe4d9b4
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-04-15 15:10:11 +0100

    dashnav fix

--- FilRouge-app/resources/views/Partials/dashnav.blade.php
+++ FilRouge-app/resources/views/Partials/dashnav.blade.php
@@ -31,20 +31,7 @@
             </div>
 
             <nav class="mt-10">
-                <a class="flex items-center px-6 py-2 mt-4 text-gray-100 bg-gray-700 bg-opacity-25" href="#">
-                    <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"></path>
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path>
-                    </svg>
-
-                    <span class="mx-3">Dashboard</span>
-                </a>
-
-                <a class="flex items-center px-6 py-2 mt-4 text-gray-100 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
-                   href="#">
+                <a class="flex items-center px-6 py-2 mt-4 text-gray-100 bg-opacity-25" href="/dashboard">
                     <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                          stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -52,11 +39,12 @@
                         </path>
                     </svg>
 
-                    <span class="mx-3">UI Elements</span>
+                    <span class="mx-3">Users</span>
                 </a>
 
+
                 <a class="flex items-center px-6 py-2 mt-4 text-gray-100 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
-                   href="#">
+                   href="/Dash_Product">
                     <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                          stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -64,7 +52,7 @@
                         </path>
                     </svg>
 
-                    <span class="mx-3">Tables</span>
+                    <span class="mx-3">Products</span>
                 </a>
 
                 <a class="flex items-center px-6 py-2 mt-4 text-gray-100 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
