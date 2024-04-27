f542830486a71f5d66f004fde39b8219f8c8ae22
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-04-27 10:00:01 +0100

    hfhf

--- FilRouge-app/resources/views/reviews.blade.php
+++ FilRouge-app/resources/views/reviews.blade.php
@@ -32,6 +32,7 @@
                       rows="5" required>
                   </textarea>
                 </div>
+                <div>hello</div>
                 <div class="mb-4 col-span-1 md:col-span-3">
                   <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                          id="multiple_files" type="file" multiple>
